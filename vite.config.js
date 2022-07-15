91ac63de67a7c0c99d5732f56825f277b94c6d5a
xieyongxuan <buaa16xyx@163.com>
2022-07-15 14:39:34 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // 无法提供 https 服务
   // base: 'http://static.moxuan.site/cra-deploy/'
-  base: 'https://moxuan-static.oss-cn-beijing.aliyuncs.com/cra-deploy/',
+  // base: 'https://moxuan-static.oss-cn-beijing.aliyuncs.com/cra-deploy/',
 })
